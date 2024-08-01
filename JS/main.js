18a48a5babb73c31449b569d833bfa0b543e49b7
GuyMor89 <guymor89@gmail.com>
2024-08-02 02:25:23 +0300

    First commit

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+'use strict'
+
+
+const gElCanvas = document.querySelector('canvas')
+const CTX = gElCanvas.getContext('2d')
+
+CTX.fillStyle = 'grey'
+CTX.fillRect(0, 0, gElCanvas.width, gElCanvas.height)
+
